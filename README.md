563e13a8ad8923a9d302ade773ef7881d75c5f97
wdrz <wtddrz@gmail.com>
2020-05-30 14:49:21 +0200

    Dodanie README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Projekt 2
+## Symulacja wyborów
+Programowanie Obiektowe, 2020-05-30, Witold Drzewakowski
+
+## Uruchamianie
+Główna klasa projektu to Main. Nazwa pliku z danymi wyjściowymi należy podać jako argument
+
--- wybory/Main.java
+++ wybory/Main.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String args[]) throws FileNotFoundException, IncorrectInputException {
-        Input in = new Input("test_own.txt");
+        Input in = new Input(args[0]);
         Bajtocja baj = in.wczytaj();
         baj.przeprowadzKampanie();
         baj.przeprowadzGlosowanieIWypisz();
